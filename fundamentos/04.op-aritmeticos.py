c73693704326c14e286661d5952bc2e896305fd2
limferth <limferth@gmail.com>
2024-05-25 00:17:30 -0400

    aprendimos sobre operadores aritmeticos y operadores logicos

---
+++ fundamentos/04.op-aritmeticos.py
@@ -0,0 +1,24 @@
+#SUMA Y RESTA
+suma = 12 + 5
+resta = 12 - 2
+
+#multiplicacion
+multiplicacion = 12 * 5
+division = 12 / 5 # la division nos devuelve float
+
+#POTENCIACION (EXPONENTE) (**)
+exponente = 12**5
+
+#division baja //
+division_baja = 12 // 5 #devuelve entero  hacia abajo
+
+#Resto o modulo
+
+resto = 12 % 5
+
+print("12 * 5 es :", multiplicacion)
+print("12 / 5 es : ", division)
+print("12 + 5 es : ", suma)
+print("12 // es : ", division_baja)
+print("soy el resto de 12 % 5 = ", resto)
+print("soy el esxponente de 12 * 5 = ", exponente)
---
+++ fundamentos/05.op-comparacion.py
@@ -0,0 +1,43 @@
+#OPERADORES DE COMPRACION
+# == es mayor que
+# != es distinto de 
+# < es menor que
+# <= es menor o igual que
+# > es mayor que
+# >= es mayor o igual que
+
+es_igual_que = 5 == 6
+
+es_distinto_de = 5 != 6
+
+mayor_que = 5 > 6
+menor_que = 5 <6 
+
+menor_o_igual = 5 <= 6
+
+mayor_o_igual = 5 <= 6
+
+print(es_igual_que)
+print(es_distinto_de)
+print(mayor_que)
+print(menor_que)
+
+#Ejercicio 
+a = 5
+b = 10
+c = 20
+
+compracion = a + b < c
+print(" 5 + 10 es menor que 20", compracion)
+
+
+# ejercicio 2 comprara usuario
+ucontrasena_almacenada = "Limber Fernando"
+contrasena = "Limberth"
+
+print(ucontrasena_almacenada == contrasena)
+
+
+
+
+
