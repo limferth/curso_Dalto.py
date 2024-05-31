0cbba04e67011c3df30f70d4384e24d1eb87fdf5
limferth <limferth@gmail.com>
2024-05-30 20:08:35 -0400

    aprendimos mas sobre las tuplas

---
+++ 03-sec-intermedia/02-tuplas.py
@@ -0,0 +1,12 @@
+
+#creado tuplas con tuple
+
+tupla = tuple(["dato1", "dato2"])
+
+#creando una tupla sin parentesis de multiples datos
+tupla = "dato1", "dato2" # las tuplas tambien se pueden crear sin parentesis
+
+#creando una tupla sin parentesis de un solo dato, la coma es importante siemrpe.
+tupla = "datoddd",
+
+print(type(tupla))
