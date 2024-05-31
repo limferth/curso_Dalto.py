0d149bb7b52e0f1d3413d8157cd35d0ee0fd0f8f
limferth <limferth@gmail.com>
2024-05-31 02:07:04 -0400

    diccionarios aprofundidad

--- 03-sec-intermedia/04-diccionarios.py
+++ 03-sec-intermedia/04-diccionarios.py
@@ -3,7 +3,17 @@ diccionario = dict(nombre = "limber", apellido="alanoca")
 
 
 
-#las listas no pueden er claves
-diccionario = {["dalto", "genio"]:"sdsds"}
+#las listas no pueden ser claves
+diccionario = {("dalto", "genio"):"sdsds"}
+
+diccionario1 = {frozenset(["ddalto", "ggenio"]):"sdsdffs"}
+
+#creando diccionarios con fromkeys() valor por defecto none
+diccionario= dict.fromkeys("nombre", "apellido")
+
+#creando diccionarios con fromkeys() cambiando el valor por defecto a nose
+diccionario1= dict.fromkeys(["nombre", "apellido"], "no se")
+
 
 print(diccionario)
+print(diccionario1)
