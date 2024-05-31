5140ddbbda764fb22a4d7fb701dc1a589da4ffec
limferth <limferth@gmail.com>
2024-05-30 20:09:49 -0400

    aprendimos mas sobre los diccionarios y su aplicacion

---
+++ 03-sec-intermedia/04-diccionarios.py
@@ -0,0 +1,9 @@
+#creando diccionarios con dict()
+diccionario = dict(nombre = "limber", apellido="alanoca")
+
+
+
+#las listas no pueden er claves
+diccionario = {["dalto", "genio"]:"sdsds"}
+
+print(diccionario)
