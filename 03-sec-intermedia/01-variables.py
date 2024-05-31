25005b7e1738a127a6d268bf48f280bed28518e0
limferth <limferth@gmail.com>
2024-05-30 20:07:58 -0400

    aprendimos sobre variables de python a profundidad

---
+++ 03-sec-intermedia/01-variables.py
@@ -0,0 +1,10 @@
+#DESEMPAQUETADO DE VARIABLES
+
+datos_en_tupla = ("lucas", "dalto", 12)
+datos_en_lista = ["lucas", "dalto", 12]
+
+nombre,apellido,suscriptores = datos_en_lista
+
+#mostrando resultados
+print(suscriptores)
+
